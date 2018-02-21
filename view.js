51a5f9a87020cae61777082aa1ba7c91a2a325ef
gavicus <gavinlowry@gmail.com>
2018-02-20 18:50:54 -0700

    clicking a hex moves the map focus to that hex

--- view.js
+++ view.js
@@ -1,9 +1,19 @@
 class View {
 	constructor () {
+		this.debug = false;
 		this.canvas = $('#canvas').get(0);
 		console.log('canvas',this.canvas);
 		this.context = this.canvas.getContext('2d');
+
+		if(!this.debug){
+			$('#canvas').width=400;
+			$('#canvas').height=400;
+		}
 		this.map = new Map(this.context, this.canvas.width, this.canvas.height);
+		if(this.debug){
+			this.map.colors.hexLine = '#eee';
+			this.map.colors.water = '#eee';
+		}
 	}
 	clear(){
 		let c = this.context;
